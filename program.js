3e41ec831063af3a8b006f615bdd45c46ad06651
kvelraj_concord <kvelraj@concord.net>
2021-09-01 10:42:48 +0530

    js

---
+++ program.js
@@ -0,0 +1,14 @@
+// Number -> Any value from Positive, Negative, Fractional, Decimal
+
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var roll_number = 1547;
+
+console.log(roll_number);
+
+var name = "Karrhick Kumar";
+
+console.log(name);
+
