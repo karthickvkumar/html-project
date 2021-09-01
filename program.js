f74f08bae27fc534ec60f29c89a65d8dfc32500b
kvelraj_concord <kvelraj@concord.net>
2021-09-01 10:56:25 +0530

    js

--- program.js
+++ program.js
@@ -5,10 +5,19 @@
 // variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
 
 var roll_number = 1547;
-
 console.log(roll_number);
 
-var name = "Karrhick Kumar";
-
+var name = "Karthick Kumar";
 console.log(name);
+
+var result = true;
+console.log(result);
+
+var student_information = {
+  name : "Karthick",
+  age : 27,
+  result : true,
+  roll_number : 4578
+}
+console.log(student_information);
 
